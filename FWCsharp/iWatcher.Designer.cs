d1fbe1d485d40168bba2d312d87e9d79df25aebf
Hamad Khawaja <khawajahamad217@gmail.com>
2016-12-20 12:06:31 -0500

    File Watcher .tga

---
+++ FWCsharp/iWatcher.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace FWCsharp
+{
+    partial class iWatcher
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox_activity = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Disable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(230, 11);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_browse.TabIndex = 12;
+            this.btn_browse.Text = "Browse";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+            // 
+            // txt_path
+            // 
+            this.txt_path.Location = new System.Drawing.Point(21, 13);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.Size = new System.Drawing.Size(203, 20);
+            this.txt_path.TabIndex = 11;
+            this.txt_path.TextChanged += new System.EventHandler(this.txt_path_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(325, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(211, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox_activity
+            // 
+            this.listBox_activity.FormattingEnabled = true;
+            this.listBox_activity.Location = new System.Drawing.Point(24, 80);
+            this.listBox_activity.Name = "listBox_activity";
+            this.listBox_activity.Size = new System.Drawing.Size(281, 160);
+            this.listBox_activity.TabIndex = 15;
+            this.listBox_activity.SelectedIndexChanged += new System.EventHandler(this.listBox_activity_SelectedIndexChanged);
+            // 
+            // iWatcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBox_activity);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "iWatcher";
+            this.Size = new System.Drawing.Size(550, 250);
+            this.Load += new System.EventHandler(this.iWatcher_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox listBox_activity;
+    }
+}
---
+++ FWCsharp/iWatcher.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace FWCsharp
+{
+
+    public partial class iWatcher : UserControl
+    {
+        string watched;
+        FileSystemWatcher fsw = new FileSystemWatcher();
+        string imageLocation;
+        int bclick;
+        public iWatcher()
+        {
+            InitializeComponent();
+        }
+
+        public void filewatcher()
+        {
+            string pattern = @"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$";
+            if (checkBox1.Checked == false)
+            {
+                if (Regex.IsMatch(txt_path.Text, pattern)==true)
+                {
+                    if (Directory.Exists(txt_path.Text))
+                    {
+                        
+                        fsw.Path = txt_path.Text;
+                        watched = fsw.Path;
+
+                        fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                          | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+                        fsw.Filter = "*.*";
+
+                        fsw.Changed += new FileSystemEventHandler(OnChanged);
+                        fsw.Created += new FileSystemEventHandler(OnChanged);
+                        fsw.Deleted += new FileSystemEventHandler(OnChanged);
+
+                        fsw.EnableRaisingEvents = true;
+
+                        string[] files = Directory.GetFiles(watched);
+
+                        foreach (var file in files)
+                        {
+                            if (System.IO.Path.GetExtension(file) == ".png" || System.IO.Path.GetExtension(file) == ".jpg" || System.IO.Path.GetExtension(file) == ".tga")
+                            {
+                                listBox_activity.Items.Insert(0, Path.GetFileName(file));
+                                listBox_activity.SelectedIndex = 0;
+                            }
+                            else
+                                pictureBox1.Image = null; 
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void OnChanged(object source, System.IO.FileSystemEventArgs e)
+        {
+
+            try
+            {
+                if (e.ChangeType == WatcherChangeTypes.Created)
+                {
+                    this.Invoke((MethodInvoker)(() => listBox_activity.Items.Insert(0, Path.GetFileName(e.FullPath))));
+                    this.Invoke((MethodInvoker)(() => listBox_activity.SelectedIndex = 0));
+                }
+
+                if (e.ChangeType == WatcherChangeTypes.Deleted)
+                {
+                    this.Invoke((MethodInvoker)(() => listBox_activity.Items.Remove(Path.GetFileName(e.FullPath))));
+                    if (listBox_activity.Items.Count !=0)
+                    {
+                        this.Invoke((MethodInvoker)(() => listBox_activity.SelectedIndex = 0));
+                    }
+                 }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void iWatcher_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btn_browse_Click(object sender, EventArgs e)
+        {
+            browse(); 
+        }
+
+        private void txt_path_TextChanged(object sender, EventArgs e)
+        {
+            filewatcher();
+        }
+
+        private void listBox_activity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                imageLocation = watched + @"\" + listBox_activity.SelectedItem;
+                if (Path.GetExtension(imageLocation) == ".tga")
+                {
+                    pictureBox1.Image = Paloma.TargaImage.LoadTargaImage(imageLocation);
+                }
+                else
+                    pictureBox1.ImageLocation = imageLocation;
+            }
+            catch
+            {
+            }
+        }
+
+        public void browse()
+        {
+            if (bclick == 0)
+            {
+                btn_browse.Enabled = true;
+                listBox_activity.Items.Clear();
+                bclick = bclick + 1;
+            }
+            else
+            {
+                btn_browse.Enabled = true;
+                listBox_activity.Items.Clear();  
+                pictureBox1.Image = null;
+
+                if (listBox_activity.Items.Count != 0)
+                {
+                    fsw.EnableRaisingEvents = false;
+                }
+            }
+
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txt_path.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+        }
+
+        public void disable()
+        {
+            listBox_activity.Items.Clear();
+            pictureBox1.Image = null;
+            fsw.EnableRaisingEvents = false; 
+        }
+
+        public void enable()
+        {
+            listBox_activity.Items.Clear();
+            filewatcher();   
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                if (checkBox1.Checked == true)
+                {
+                    disable();
+                }
+
+                if (checkBox1.Checked == false)
+                {
+                    enable();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
